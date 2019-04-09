a8fb91af9404b2af34ff90521f052de7ed63115c
kodak <2270301642@qq.com>
2019-04-09 14:51:26 +0800

    Initial commit

---
+++ calffinancing/src/main/java/com/ht/calffinancing/CalffinancingApplication.java
@@ -0,0 +1,13 @@
+package com.ht.calffinancing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalffinancingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalffinancingApplication.class, args);
+    }
+
+}
